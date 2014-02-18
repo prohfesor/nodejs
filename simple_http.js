9d15161ad413ff624086abc12aa6903970854336
Pavel Dvoinos <p.dvoynos@gmail.com>
2014-02-18 16:57:08 +0200

    simple

---
+++ simple_http.js
@@ -0,0 +1,12 @@
+/**
+ * Created by prof on 18.02.14.
+ */
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Hello Http');
+});
+
+server.listen(8080);
